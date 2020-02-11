6449e7da381e78c6a851988ee2b3e66826ef7b1c
Jameson Hodge <jamesondh@gmail.com>
2020-02-11 18:00:20 -0500

    Fix foreign key error

--- hello/models.py
+++ hello/models.py
@@ -12,14 +12,13 @@ class Users(models.Model):
 
 class Jobs(models.Model):
 	job_id = models.AutoField(primary_key=True)
-	user_id = models.ForeignKey('Users')
+	user_id = models.ForeignKey('Users', on_delete=models.CASCADE)
 	input_text = models.CharField(max_length=50)
 	created_date = models.DateTimeField("date created", auto_now_add=True)
 	completed_date = models.DateTimeField("date completed", auto_now_add=False)
 
 class Job_Results(models.model):
 	result_id = models.AutoField(primary_key=True)
-	job_id = models.ForeignKey('Jobs')
+	job_id = models.ForeignKey('Jobs', on_delete=models.CASCADE)
 	sentiment_score = models.FloatField()
 	executed_date = models.DateTimeField("date executed", auto_now_add=False)
-	
